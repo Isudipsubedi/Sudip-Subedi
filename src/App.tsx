46e1469efd60277bf44a1060f204353045db8db6
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-03-14 20:41:27 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Sudip-Subedi">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
