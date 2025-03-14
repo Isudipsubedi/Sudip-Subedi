87fbeae077b41e04b061ed28b40834d1eab8a8c9
Sudip Subedi <88761244+Isudipsubedi@users.noreply.github.com>
2025-03-14 20:47:47 +0545

    "vite.cofig.ts updated"

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Sudip-Subedi/",  // Add this line to set the correct base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
